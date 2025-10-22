12aaea560dc88b6c77dd478515da2e4996e2d423
kokjp1 <james.kok@hva.nl>
2025-10-22 16:00:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    host: '127.0.0.1',   // bind explicit op 127.0.0.1 (ipv ::1 of localhost)
-    port: 5173,          // zelfde poort als je redirect
+    // host: '127.0.0.1',   // bind explicit op 127.0.0.1 (ipv ::1 of localhost)
+    // port: 5173,          // zelfde poort als je redirect
     strictPort: true     // fail als 5173 bezet is (zodat je het merkt)
   }
 });
